50f70470ef8e355755ebf9a726887c91c7852bba
Tarsis <br.tmvdl@gmail.com>
2023-11-09 11:37:34 -0300

    Updating INDEX page file

--- src/www/html/index.js
+++ src/www/html/index.js
@@ -1,17 +1,66 @@
-import { HTML, nLink } from '@brtmvdl/frontend'
+import { HTML, nFlex, nImage } from '@brtmvdl/frontend'
 import { TopBar } from './components/topbar.js'
 
+import { KeyValuePair } from './utils/classes.js'
+
 export class Page extends HTML {
-  onCreate() {
-    this.append(new TopBar())
-    this.append(this.getLink('Add funds', '/funds/add.html'))
-    this.append(this.getLink('History', '/history.html'))
+  children = {
+    exchanges_list: new HTML(),
+    coins_table: new HTML(),
   }
 
-  getLink(text, href) {
-    const link = new nLink()
-    link.setText(text)
-    link.href(href)
-    return link
+  state = {
+    exchange: 'binance',
+    exchanges: [
+      new KeyValuePair('binance', 'Binance'),
+      new KeyValuePair('foxbit', 'Foxbit'),
+      new KeyValuePair('bybit', 'Bybit'),
+    ],
+  }
+
+  onCreate() {
+    this.append(new TopBar())
+    const container = new HTML()
+    container.setStyle('margin', '0 auto')
+    container.setStyle('width', '40rem')
+    container.append(this.getExchangesList())
+    container.append(this.getCoinsTable())
+    this.append(container)
+    //
+    this.updateValues()
+  }
+
+  updateValues() {
+    const self = this
+    setInterval(() => console.log({ exchange: self.state.exchange }), 1000)
+  }
+
+  getExchangesList() {
+    const flex = new nFlex()
+
+    this.state.exchanges.map(({ key, value }) => {
+      const exchange = new HTML()
+      exchange.on('click', () => { this.state.exchange = key })
+
+      const image = new nImage()
+      image.src(`/img/${key}.png`)
+      image.setStyle('max-width', '200px')
+      exchange.append(image)
+
+      const text = new HTML()
+      text.setText(value)
+      text.setStyle('text-align', 'center')
+      exchange.append(text)
+
+      flex.append(exchange)
+    })
+
+    this.children.exchanges_list.append(flex)
+
+    return this.children.exchanges_list
+  }
+
+  getCoinsTable() {
+    return this.children.coins_table
   }
 }
