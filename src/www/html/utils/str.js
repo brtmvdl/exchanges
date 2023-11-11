52e679d50107453ffc3451d7fadb8422bc104673
Tarsis <br.tmvdl@gmail.com>
2023-11-11 16:58:23 -0300

    Updating www files

--- src/www/html/utils/str.js
+++ src/www/html/utils/str.js
@@ -7,6 +7,14 @@ export const padLeft = (text = '', length = 1, pad = ' ') => {
   return text.toString()
 }
 
+export const padRight = (text = '', length = 1, pad = ' ') => {
+  while (text.toString().length < length) {
+    text = text.toString() + pad.toString()
+  }
+
+  return text.toString()
+}
+
 export const fixDecimals = (num) => {
   return num.toString()
     .replace(/(.)999999.*/, (_, x) => +x + 1)
@@ -15,5 +23,5 @@ export const fixDecimals = (num) => {
 
 export const price2string = (price = 0) => {
   const [bills, cents] = price.toString().split('.')
-  return `${bills},${fixDecimals(padLeft(cents, 2, '0'))}`
+  return `${bills},${fixDecimals(padRight(cents, 2, '0'))}`
 }
